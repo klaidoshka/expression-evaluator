a5b85ddb43f0ba8f3b28f12a39f0fbe7e66ccf89
klaidas <klaidukast17@gmail.com>
2025-01-28 17:58:13 +0200

    Add jitpack publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    `maven-publish`
 }
 
 group = "com.github.klaidoshka"
 version = "1.0"
 
-repositories { 
-    mavenCentral()
-}
+repositories { mavenCentral() }
+dependencies { implementation(kotlin("stdlib-jdk8")) }
+kotlin { jvmToolchain(21) }
 
-dependencies { 
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-kotlin {
-    jvmToolchain(21)
+publishing {
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
 }
